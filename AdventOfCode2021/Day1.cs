666e2bdd96b771659c58da532e6bc5b03ccc42ec
Mark Heath <mark.heath@gmail.com>
2021-12-01 07:30:21 +0000

    day 1 part 1

--- AdventOfCode2021/Day1.cs
+++ AdventOfCode2021/Day1.cs
@@ -6,17 +6,16 @@ namespace AdventOfCode2021
 {
     public class Day1 : ISolver
     {
-        public (string, string) ExpectedResult => ("1015476", "200878544");
+        public (string, string) ExpectedResult => ("1548", "");
 
         public (string, string) Solve(string[] input)
         {
-            return ($"{Solve(input, 2)}", $"{Solve(input, 3)}");
+            return ($"{Solve(input,1)}", $"");
         }
 
-        long Solve(IEnumerable<string> input, int count) => 
+        long Solve(IEnumerable<string> input,int _) =>
             input.Select(long.Parse)
-                 .Subsets(count)
-                 .First(n => n.Sum() == 2020)
-                 .Aggregate((x, y) => x * y);
+                .Pairwise((a, b) => b - a)
+                .Count(a => a > 0);
     }
 }
--- UnitTests/Day1Tests.cs
+++ UnitTests/Day1Tests.cs
@@ -8,15 +8,19 @@ namespace UnitTests
         [Test]
         public void SolveWithTestInput()
         {
-            var testInput = @"1721
-                979
-                366
-                299
-                675
-                1456".Split("\r\n");
+            var testInput = @"199
+200
+208
+210
+200
+207
+240
+269
+260
+263".Split("\r\n");
             var day1 = new Day1();
             var solution = day1.Solve(testInput);
-            Assert.AreEqual(("514579", "241861950"), solution);
+            Assert.AreEqual(("7", ""), solution);
         }
     }
 }
