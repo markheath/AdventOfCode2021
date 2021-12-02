9e51f79935012e8f4878b96c7ca13e0367d8067d
Mark Heath <mark.heath@gmail.com>
2021-12-02 09:12:46 +0000

    using records

--- AdventOfCode2021/Day2.cs
+++ AdventOfCode2021/Day2.cs
@@ -4,6 +4,8 @@ using System.Linq;
 
 namespace AdventOfCode2021
 {
+    public record SubState(long Horizontal, long Depth, long Aim);
+
     public class Day2 : ISolver
     {
         public (string, string) ExpectedResult => ("1484118", "1463827010");
@@ -13,27 +15,30 @@ namespace AdventOfCode2021
             return ($"{Part1(input)}", $"{Part2(input)}");
         }
 
-        private Dictionary<string, (long, long)> vectors = new Dictionary<string, (long, long)>()
+
+
+        private Dictionary<string, SubState> vectors = new Dictionary<string, SubState>()
         {
-            { "forward", (1,0) },
-            { "down", (0,1) },
-            { "up", (0,-1) }
+            { "forward", new(1,0,0) },
+            { "down", new(0,1,0) },
+            { "up", new(0,-1,0) }
         };
 
         long Part1(IEnumerable<string> input) =>
             input.Select(s => s.Split(' '))
                 .Select(p => new { Dir = vectors[p[0]], Dist = long.Parse(p[1]) })
-                .Scan((0L, 0L), (cur, inst) => (cur.Item1 + inst.Dist * inst.Dir.Item1, cur.Item2 + inst.Dist * inst.Dir.Item2))
-                .Select(x => x.Item1 * x.Item2)
+                .Scan(new SubState(0,0,0), (cur, inst) => new (cur.Horizontal + inst.Dist * inst.Dir.Horizontal, 
+                    cur.Depth + inst.Dist * inst.Dir.Depth, 0))
+                .Select(x => x.Horizontal * x.Depth)
                 .Last();
 
         long Part2(IEnumerable<string> input) =>
             input.Select(s => s.Split(' '))
                 .Select(p => new { Dir = vectors[p[0]], Dist = long.Parse(p[1]) })
                 // h, d, a
-                .Scan((0L, 0L, 0L), (cur, inst) => (cur.Item1 + inst.Dist * inst.Dir.Item1, cur.Item2 + inst.Dist * inst.Dir.Item1 * cur.Item3, cur.Item3 + inst.Dist * inst.Dir.Item2))
+                .Scan(new SubState(0, 0, 0), (cur, inst) => new(cur.Horizontal + inst.Dist * inst.Dir.Horizontal, cur.Depth + inst.Dist * inst.Dir.Horizontal * cur.Aim, cur.Aim + inst.Dist * inst.Dir.Depth))
                 //.Pipe(x => System.Console.WriteLine(x))
-                .Select(x => x.Item1 * x.Item2)
+                .Select(x => x.Horizontal * x.Depth)
                 .Last();
 
     }
