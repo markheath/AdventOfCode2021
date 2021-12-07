dad3f4683b70bc70b2af834b332ddd01d2394944
Mark Heath <mark.heath@gmail.com>
2021-12-07 07:29:55 +0000

    day 7 part 1

---
+++ AdventOfCode2021/Day7.cs
@@ -0,0 +1,40 @@
+﻿using MoreLinq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode2021
+{
+
+    public class Day7 : ISolver
+    {
+        public (string, string) ExpectedResult => ("342730", "");
+
+        public (string, string) Solve(string[] input)
+        {
+            var crabPos = input[0].Split(',').Select(int.Parse).ToArray();
+
+            var minFuel = int.MaxValue;
+            var cheapestPos = -1;
+            for(int pos = crabPos.Min(); pos <= crabPos.Max(); pos++)
+            {
+                var fuel = 0;
+                for(int crab = 0; crab < crabPos.Length; crab++)
+                {
+                    fuel += Math.Abs(pos - crabPos[crab]);
+                }
+                if (fuel < minFuel)
+                {
+                    minFuel = fuel;
+                    cheapestPos = pos;
+                }
+            }
+
+            return ($"{minFuel}", $"");
+        }
+
+
+    }
+
+}
---
+++ UnitTests/Day7Tests.cs
@@ -0,0 +1,19 @@
+using AdventOfCode2021;
+using NUnit.Framework;
+using System.Linq;
+
+namespace UnitTests
+{
+    public class Day7Tests
+    {
+        [Test]
+        public void SolveWithTestInput()
+        {
+            var testInput = @"16,1,2,0,4,2,7,1,2,14".Split("\r\n");
+            var solver = new Day7();
+            var solution = solver.Solve(testInput);
+            Assert.That(solution, Is.EqualTo(("37", "")));
+        }
+
+    }
+}
