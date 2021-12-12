e89ff1336598eedbc53f45f94fb8752e774f5ea1
Mark Heath <mark.heath@gmail.com>
2021-12-12 13:58:00 +0000

    day 12 part 1

---
+++ AdventOfCode2021/Day12.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode2021
+{
+
+    public class Day12 : ISolver
+    {
+        public (string, string) ExpectedResult => ("4885", "");
+
+        public (string, string) Solve(string[] input)
+        {
+            var connections =
+            input.SelectMany(x => x.Split('-'))
+                .ToHashSet()
+                .ToDictionary(x => x, x => new HashSet<string>());
+
+
+            foreach (var connection in input.Select(x => x.Split('-')))           
+            {
+                connections[connection[0]].Add(connection[1]);
+                connections[connection[1]].Add(connection[0]);
+            }
+
+            var routeCount = FindRoutes(connections, "start").Count();
+            return ($"{routeCount}", $"");
+        }
+
+
+        // needs to be depth-first
+        private IEnumerable<string> FindRoutes(Dictionary<string, HashSet<string>> map, string from, string currentRoute = "start,")
+        {
+            // valid places to go are big caves (upper) or anywhere we've not been yet
+            foreach(var next in map[from].Where(d => char.IsUpper(d[0]) || !currentRoute.Contains(d)))
+            {
+                if (next == "end") yield return currentRoute + next;
+                else foreach (var r in FindRoutes(map, next, currentRoute + next + ",")) yield return r;
+            }
+        }
+
+    }
+}
---
+++ UnitTests/Day12Tests.cs
@@ -0,0 +1,69 @@
+using AdventOfCode2021;
+using NUnit.Framework;
+using System.Linq;
+
+namespace UnitTests
+{
+    public class Day12Tests
+    {
+        [Test]
+        public void SolveWithTestInput()
+        {
+            var testInput = @"start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end".Split("\r\n");
+            var solver = new Day12();
+            var solution = solver.Solve(testInput);
+            Assert.That(solution.Item1, Is.EqualTo("10"));
+        }
+
+        [Test]
+        public void SolveWithTestInput2()
+        {
+            var testInput = @"dc-end
+HN-start
+start-kj
+dc-start
+dc-HN
+LN-dc
+HN-end
+kj-sa
+kj-HN
+kj-dc".Split("\r\n");
+            var solver = new Day12();
+            var solution = solver.Solve(testInput);
+            Assert.That(solution.Item1, Is.EqualTo("19"));
+        }
+
+        [Test]
+        public void SolveWithTestInput3()
+        {
+            var testInput = @"fs-end
+he-DX
+fs-he
+start-DX
+pj-DX
+end-zg
+zg-sl
+zg-pj
+pj-he
+RW-he
+fs-DX
+pj-RW
+zg-RW
+start-pj
+he-WI
+zg-he
+pj-fs
+start-RW".Split("\r\n");
+            var solver = new Day12();
+            var solution = solver.Solve(testInput);
+            Assert.That(solution.Item1, Is.EqualTo("226"));
+        }
+
+    }
+}
