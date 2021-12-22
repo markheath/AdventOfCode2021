bf3d6751c3d6ba1fb2330e1d82e66f3cc321bed2
Mark Heath <mark.heath@gmail.com>
2021-12-22 07:56:03 +0000

    Day 22 part 1

--- AdventOfCode2021/Coord.cs
+++ AdventOfCode2021/Coord.cs
@@ -70,4 +70,74 @@ namespace AdventOfCode2021
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
 
+    public struct Coord3 : IEnumerable<int>, IEquatable<Coord3>
+    {
+        private readonly int x;
+        private readonly int y;
+        private readonly int z;
+        public int X { get => x; }
+        public int Y { get => y; }
+        public int Z { get => z; }
+
+        public int this[int index]
+        {
+            get { return index == 0 ? x : y; }
+        }
+
+        public Coord3(int x, int y, int z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public static implicit operator (int, int, int)(Coord3 c) => (c.x, c.y, c.z);
+        public static implicit operator Coord3((int X, int Y, int Z) c) => new Coord3(c.X, c.Y, c.Z);
+
+        public void Deconstruct(out int x, out int y)
+        {
+            x = this.x;
+            y = this.y;
+        }
+
+        public static Coord3 operator +(Coord3 a, Coord3 b)
+        {
+            return new Coord3(a.x + b.x, a.y + b.y, a.z + b.z);
+        }
+
+        public override bool Equals(object other) =>
+            other is Coord3 c
+                && c.x.Equals(x)
+                && c.y.Equals(y)
+                && c.z.Equals(z);
+
+        // Implement IEquatable<T> https://stackoverflow.com/a/8952026/7532
+        public bool Equals([AllowNull] Coord3 other) => x == other.x && y == other.y && z == other.z;
+
+
+        public override int GetHashCode()
+        //            => HashHelpers.Combine(HashHelpers.Combine(HashHelpers.Combine(HashHelpers.RandomSeed, x), y), z);
+        {
+            // based on Jon Skeet - hashcode of an int is just its value
+            unchecked // Overflow is fine, just wrap
+            {
+                int hash = 17;
+                hash = hash * 29 + x;
+                hash = hash * 29 + y;
+                hash = hash * 29 + z;
+                return hash;
+            }
+        }
+
+        public override string ToString() => $"({x},{y},{z})";
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            yield return x;
+            yield return y;
+            yield return z;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
 }
---
+++ AdventOfCode2021/Day22.cs
@@ -0,0 +1,36 @@
+﻿using MoreLinq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2021;
+
+
+public class Day22 : ISolver
+{
+    public (string, string) ExpectedResult => ("", "");
+    
+    // record RebootStep(bool targetState, )
+
+    public (string Part1, string Part2) Solve(string[] input)
+    {
+        var rebootSteps = input.Select(line => new { On = line[1] == 'n', Points = Regex.Matches(line,@"-?\d+").Select(m => Int32.Parse(m.Value)).ToArray() }).ToArray();
+        var onCubes = new HashSet<Coord3>();
+        foreach(var step in rebootSteps.Where(s => s.Points.All(p => p <= 50 && p >= -50)))
+        {
+            for(var x = step.Points[0]; x <= step.Points[1]; x++)
+                for (var y = step.Points[2]; y <= step.Points[3]; y++)
+                    for (var z = step.Points[4]; z <= step.Points[5]; z++)
+                        
+                        if (step.On)
+                            onCubes.Add((x,y,z));
+                        else
+                            onCubes.Remove((x,y,z));
+        }
+
+        return ($"{onCubes.Count}", $"");
+    }
+
+    
+}
